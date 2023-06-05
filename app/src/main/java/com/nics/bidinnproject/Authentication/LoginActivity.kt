a83ea95692aa40da6c859602a384239beb0774b1
naeem-2005 <naeemkhan7d@gmail.com>
2023-06-05 13:27:32 +0530

    For practice sample

--- app/src/main/java/com/nics/bidinnproject/Authentication/LoginActivity.kt
+++ app/src/main/java/com/nics/bidinnproject/Authentication/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.nics.bidinnproject.Authentication
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
@@ -11,18 +12,24 @@ import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.text.style.LeadingMarginSpan
 import android.text.style.StyleSpan
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
+import com.google.android.material.card.MaterialCardView
 import com.nics.bidinnproject.MainActivity
 import com.nics.bidinnproject.R
 
 class LoginActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val editTextMobile: EditText = findViewById(R.id.editTextMobile)
         val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
+        val OtpCard = findViewById<MaterialCardView>(R.id.otpcard)
+        val background = findViewById<RelativeLayout>(R.id.background)
         val hint = "+91 | Mobile Number"
 
         val spannableString = SpannableString(hint)
@@ -37,14 +44,16 @@ class LoginActivity : AppCompatActivity() {
 
         editTextMobile.hint = spannableString
 
-
+        OtpCard.visibility = View.INVISIBLE
 
         buttonSubmit.setOnClickListener {
             // Create a Dialog or a custom view with transparent background
             val intent = Intent(this, MainActivity::class.java)
 
             // Start the new activity
-            startActivity(intent)
+            OtpCard.visibility = View.VISIBLE
+            background.setBackgroundColor(Color.parseColor("#c0c0c0"))
+
         }
 
 
---
+++ app/src/main/java/com/nics/bidinnproject/Authentication/OtpVerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.nics.bidinnproject.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nics.bidinnproject.R
+
+class OtpVerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp_verification)
+    }
+}
